0110ea6aee7284eae0e80bb66086ffe567530962
sakshi-dhakne <vaishujaybhaye10@gmail.com>
2026-06-18 00:15:42 +0530

    Initial setup and RSS news fetching

--- fetcher.py
+++ fetcher.py
@@ -1,22 +1,26 @@
 import feedparser
 
+
 def get_news():
-    news_list = []
 
     feeds = {
-        "BBC": "http://feeds.bbci.co.uk/news/rss.xml",
-        "Reuters": "https://feeds.reuters.com/reuters/topNews",
-        "CNN": "http://rss.cnn.com/rss/edition.rss"
+        "BBC": "https://feeds.bbci.co.uk/news/rss.xml",
+        "CNN": "http://rss.cnn.com/rss/edition.rss",
+        "NYTimes": "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
     }
 
+    all_news = []
+
     for source, url in feeds.items():
+
         feed = feedparser.parse(url)
 
-        for article in feed.entries[:5]:
-            news_list.append({
+        for article in feed.entries[:10]:
+
+            all_news.append({
                 "source": source,
                 "title": article.title,
                 "link": article.link
             })
 
-    return news_list
+    return all_news
