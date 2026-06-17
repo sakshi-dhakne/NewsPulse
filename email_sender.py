b18b5536e4a27b5a46dbe43d73426f0d98394c0e
sakshi-dhakne <vaishujaybhaye10@gmail.com>
2026-06-18 00:19:13 +0530

    Implemented email digest delivery

---
+++ email_sender.py
@@ -0,0 +1,35 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_email(message):
+
+    sender_email = "sakshidhakne5@gmail.com"
+
+    app_password = "wvhq fwow mjrd gxhp"
+
+    receiver_email = "vaishujaybhaye10@gmail.com"
+
+    msg = MIMEText(message)
+
+    msg["Subject"] = "Daily News Digest"
+
+    msg["From"] = sender_email
+
+    msg["To"] = receiver_email
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+
+    server.starttls()
+
+    server.login(sender_email, app_password)
+
+    server.sendmail(
+        sender_email,
+        receiver_email,
+        msg.as_string()
+    )
+
+    server.quit()
+
+    print("Email sent successfully")
